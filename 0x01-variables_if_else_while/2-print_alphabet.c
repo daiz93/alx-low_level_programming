2714229e649bf0cff0ff00f29a385769fece7ae3
Desmond KPOHIZOUN <desmond.kpo@gmail.com>
2022-03-11 22:07:43 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet char
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+    char ch = 'a';
+  
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+	putchar(ch);
+    }
+    putchar('\n');
+    return (0);
+}
