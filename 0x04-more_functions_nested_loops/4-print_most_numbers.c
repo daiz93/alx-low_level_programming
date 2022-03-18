e180617168621318980baedf7691ccb173daeb35
Desmond KPOHIZOUN <desmond.kpo@gmail.com>
2022-03-17 21:58:38 -0700

    Update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,8 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 && i != 4) 
-		_putchar(i + 48);
+		if (i != 2 && i != 4)
+			_putchar(i + 48);
 	}
 	_putchar('\n');
 }
