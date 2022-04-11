b5ef07db810bfb11abf3fa9cd0fae476181d9681
daiz93 <desmond.kpo@gmail.com>
2022-04-11 15:58:41 +0100

    a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-
-int main(int argc __attribute__((unused)), char *argv[])
+int main(void)
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", __FILE__);
 	return (0);
 }
