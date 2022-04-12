ebc8eaa500f7bbf3c62a499077baded7f04beaaa
Desmond KPOHIZOUN <desmond.kpo@gmail.com>
2022-04-12 10:29:41 -0700

    a fucntion to free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,17 @@
-#include <stdlib>
+#include <stdlib.h>
 #include "dog.h"
-#include <stdio>
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * free_dog - free dog
+ * @d: dog_t to free
  *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: notthing
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d->name);
-	free(d->age);
 	free(d->owner);
+	free(d);
 }
