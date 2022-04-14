d95781c048941ea5d761dc3ec90ae40699b74754
daiz93 <desmond.kpo@gmail.com>
2022-04-14 21:27:45 +0100

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,6 +6,7 @@
  * print_numbers -  a function that prints numbers, followed by a new line.
  * @separator: char, require parameter, is separator
  * @n: unsigned int, require parameter
+ * @...: A variable number of numbers to be printed.
  * Return: int, 0 if n == 0
  */
 
