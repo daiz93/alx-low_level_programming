77e56e44fdd9a29d7582d62b08d8df85e44bd2dc
daiz93 <desmond.kpo@gmail.com>
2022-04-15 01:04:22 +0100

    Print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <string.h>
+
+/**
+ * @opsym: the member pointing to operator symbols
+ * @f: member pointer to function
+ */
+
+typedef struct opfunc
+{
+	char *opsym;
+	void (*f)(va_list);
+} fname_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
