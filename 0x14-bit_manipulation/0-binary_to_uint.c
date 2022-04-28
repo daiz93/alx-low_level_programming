c4f7999837051234e67b3552b8d8fe5b77953a9e
daiz93 <desmond.kpo@gmail.com>
2022-04-28 20:14:43 +0100

    Function to convert binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,80 @@
 #include "main.h"
-
 /**
- * binary_to_uint - function that converts a binary 
+ * _len - get power of 2
+ * @i: number of mul 2
+ *
+ * Return: power
+ */
+
+int puissance2(unsigned int i)
+{
+        if (i == 0)
+        {
+                return (1);
+        }
+        if (i > 0)
+        {
+                return (2 * puissance2(i -1));
+        }
+        else
+        {
+                return (0);
+        }
+}
+/**
+ * _len - get length of string
+ * @c: string
+ *
+ * Return: len
+ */
+
+unsigned int _len(const char *c)
+{
+        int i = 0;
+
+        if (c)
+        {
+                while (c[i] != '\0')
+                {
+                        i++;
+                }
+        }
+        return (i);
+}
+/**
+ * binary_to_uint - function that converts a binary
  * number to an unsigned int
  * @b: binarry to convert
  *
  * Return: nsigned int, converted number (success), 0(faild)
  */
 
-unsigned int binary_to_uint(const char *b)
+unsigned int
+binary_to_uint(const char *b)
 {
+        unsigned int i = 0;
+        unsigned int result = 0;
+        int lenn = _len (b);
 
+        if (b)
+        {
+                while(b[i] != '\0')
+                {
+                        if ((b[i] != '0') & (b[i] != '1'))
+                        {
+                                return (0);
+                        }
+                        if (b[i] == '1')
+                        {
+                                result += puissance2(lenn - i - 1);
+                        }
+
+                        i++;
+                }
+                return (result);
+        }
+        else
+        {
+                return (0);
+        }
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
+int _putchar(char c);
+unsigned int _len(const char *c);
+int puissance2(unsigned int i);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
