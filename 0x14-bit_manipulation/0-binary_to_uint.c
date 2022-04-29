c130a6e6937028e2dde704d26ad0b523db2b91c5
daiz93 <desmond.kpo@gmail.com>
2022-04-29 06:05:01 +0200

    Betty style fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,17 @@
 int puissance2(unsigned int i)
 {
 	if (i == 0)
-        {
-                return (1);
-        }
-        if (i > 0)
-        {
-                return (2 * puissance2(i -1));
-        }
-        else
-        {
-                return (0);
-        }
+	{
+		return (1);
+	}
+	if (i > 0)
+	{
+		return (2 * puissance2(i -1));
+	}
+	else
+	{
+		return (0);
+	}
 }
 /**
  * _len - get length of string
@@ -30,16 +30,16 @@ int puissance2(unsigned int i)
 
 unsigned int _len(const char *c)
 {
-        int i = 0;
+	int i = 0;
 
-        if (c)
-        {
-                while (c[i] != '\0')
-                {
-                        i++;
-                }
-        }
-        return (i);
+	if (c)
+	{
+		while (c[i] != '\0')
+		{
+			i++;
+		}
+	}
+	return (i);
 }
 /**
  * binary_to_uint - function that converts a binary
@@ -51,29 +51,26 @@ unsigned int _len(const char *c)
 
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int i = 0;
-        unsigned int result = 0;
-        int lenn = _len (b);
+	unsigned int i = 0;
+	unsigned int result = 0;
+	int lenn = _len (b);
 
-        if (b)
-        {
-                while(b[i] != '\0')
-                {
-                        if ((b[i] != '0') & (b[i] != '1'))
-                        {
-                                return (0);
-                        }
-                        if (b[i] == '1')
-                        {
-                                result += puissance2(lenn - i - 1);
-                        }
-
-                        i++;
-                }
-                return (result);
-        }
-        else
-        {
-                return (0);
-        }
+	if (b)
+	{
+		while(b[i] != '\0')
+		{
+			if ((b[i] != '0') & (b[i] != '1'))
+				return (0);
+			if (b[i] == '1')
+			{
+				result += puissance2(lenn - i - 1);
+			}
+			i++;
+		}
+		return (result);
+	}
+	else
+	{
+		return (0);
+	}
 }
